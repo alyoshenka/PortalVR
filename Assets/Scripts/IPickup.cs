f07e8f3167087cf92adb64a1e5cd27479b9278bd
Alexi <alexibmost@gmail.com>
2019-06-17 17:55:46 -0700

    powerup purchasing system base functional

--- Assets/Scripts/IPickup.cs
+++ Assets/Scripts/IPickup.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IPickup : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public interface IPickup
+{
+    void ApplyEffect(GameObject target); 
 }
+
+
+
+
