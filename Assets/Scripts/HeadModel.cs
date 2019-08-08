45d41ea5eacc838b4fd1bcac2c8a7d83d544eaf3
Alexi <alexibmost@gmail.com>
2019-08-07 22:56:07 -0700

    menu set up

--- Assets/Scripts/HeadModel.cs
+++ Assets/Scripts/HeadModel.cs
@@ -7,6 +7,8 @@ using VRTK.Highlighters;
 [RequireComponent(typeof(VRTK_InteractObjectHighlighter))]
 public class HeadModel : VRTK_InteractableObject
 {
+    public int layer = 14;
+
     Player p;
 
     private void Start()
@@ -36,6 +38,8 @@ public class HeadModel : VRTK_InteractableObject
         GameObject h = Instantiate(gameObject, p.transform.position,
             p.transform.rotation, p.transform.parent);
         h.GetComponent<VRTK_InteractObjectHighlighter>().enabled = false;
+        // foreach(GameObject g in h.transform.GetComponentsInChildren<GameObject>()) { g.layer = layer; }
+        h.gameObject.layer = layer;
         //p.head = h;
         HeadModelSetter.head = h.GetComponent<HeadModel>();
 
