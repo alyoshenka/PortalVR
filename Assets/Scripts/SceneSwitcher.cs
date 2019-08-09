dbb1a92723e2a08eeaee8cb8be400390f1d9c61b
Alexi <alexibmost@gmail.com>
2019-08-08 22:20:33 -0700

    i'm done

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -33,4 +33,13 @@ public class SceneSwitcher : MonoBehaviour
     {
         SwitchScene();
     }
+
+    private void Update()
+    {
+        // reload
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
 }
